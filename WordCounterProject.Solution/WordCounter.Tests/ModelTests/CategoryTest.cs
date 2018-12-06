6bde8d2f0fd14f5e9764bab3e33a9d0a3a92d312
Student <example@example.com>
2018-12-05 19:55:47 -0800

    passed test Category.cs

---
+++ WordCounterProject.Solution/WordCounter.Tests/ModelTests/CategoryTest.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter.Models;
+using System.Collections.Generic;
+using System;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class CategoryTest
+  {
+
+    public void Dispose()
+    {
+      // Category.ClearAll();
+    }
+
+    // [TestMethod]
+    // public void CategoryConstructor_CreatesInstanceOfCategory_Category()
+    // {
+    //   // Category newCategory = new Category("test category");
+    //   Assert.AreEqual(typeof(Category), newCategory.GetType());
+    // }
+
+  }
+}
