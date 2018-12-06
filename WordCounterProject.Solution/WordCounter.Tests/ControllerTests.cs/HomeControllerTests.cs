c985b630aab6c13eec942e2dd13ca6d3603113b9
Student <example@example.com>
2018-12-05 19:43:04 -0800

    pass HomeControllerTests.cs

---
+++ WordCounterProject.Solution/WordCounter.Tests/ControllerTests.cs/HomeControllerTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WordCounter.Controllers;
+using WordCounter.Models;
+
+namespace WordCounter.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+      [TestMethod]
+      public void Index_ReturnsCorrectView_True()
+      {
+        //Arrange
+        HomeController controller = new HomeController();
+        //Act
+        var indexView = controller.Index();
+        //Assert
+        Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+      }
+    }
+}
